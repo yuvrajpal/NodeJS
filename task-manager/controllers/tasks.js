e91bcc27816fb6a70749538feff6aa5157e12fbd
Yuvraj Pal <techyuvraj@gmail.com>
2023-06-02 00:56:34 +0530

    basic validation and get and create task

--- task-manager/controllers/tasks.js
+++ task-manager/controllers/tasks.js
@@ -1,12 +1,21 @@
 const Task = require("../models/Task");
 
-const getAllTasks = (req, res) => {
-  res.send("all items from file");
+const getAllTasks = async (req, res) => {
+  try {
+    const tasks = await Task.find({});
+    res.status(200).json({ tasks });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
 const createTask = async (req, res) => {
-  const task = await Task.create(req.body);
-  res.status(210).json({ task });
+  try {
+    const task = await Task.create(req.body);
+    res.status(210).json({ task });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
 const getTask = (req, res) => {
--- task-manager/models/Task.js
+++ task-manager/models/Task.js
@@ -1,8 +1,16 @@
 const mongoose = require("mongoose");
 
 const TaskSchema = new mongoose.Schema({
-  name: String,
-  completed: Boolean,
+  name: {
+    type: String,
+    required: [true, "name is required"],
+    trim: true,
+    maxlength: [50, "name cannot be more than 50 charecters"],
+  },
+  completed: {
+    type: Boolean,
+    default: false,
+  },
 });
 
 module.exports = mongoose.model("Task", TaskSchema);
