41028834762163c214b2a2f4143e50b15ca8c5e3
Yuvraj Pal <techyuvraj@gmail.com>
2023-06-02 00:01:07 +0530

    adding env file. .env file should not be committed but we are doing that for now.

--- task-manager/app.js
+++ task-manager/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const tasks = require("./routes/tasks");
 const connectDB = require("./db/connect");
+require("dotenv").config();
 
 // middlewares
 app.use(express.json());
@@ -17,7 +18,7 @@ const port = 3000;
 
 const start = async () => {
   try {
-    await connectDB();
+    await connectDB(process.env.MONGO_URI);
     app.listen(port, console.log(`server is listening on port ${port}`));
   } catch (error) {
     console.log(error);
--- task-manager/db/connect.js
+++ task-manager/db/connect.js
@@ -1,10 +1,7 @@
 const mongoose = require("mongoose");
 
-const connectionString =
-  "mongodb+srv://expressapps:1234@cluster0.wwugs.mongodb.net/task-manager?retryWrites=true&w=majority";
-
 const connectDB = (url) => {
-  return mongoose.connect(connectionString);
+  return mongoose.connect(url);
 };
 
 module.exports = connectDB;
