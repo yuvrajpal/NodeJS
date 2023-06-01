132700daaddaa6e782890eacf8f0fdc03bd10174
Yuvraj Pal <techyuvraj@gmail.com>
2023-06-01 23:05:41 +0530

    sync db connection

--- task-manager/app.js
+++ task-manager/app.js
@@ -1,7 +1,7 @@
-require("./db/connect");
 const express = require("express");
 const app = express();
 const tasks = require("./routes/tasks");
+const connectDB = require("./db/connect");
 
 // middlewares
 app.use(express.json());
@@ -15,4 +15,13 @@ app.use("/api/v1/tasks", tasks);
 
 const port = 3000;
 
-app.listen(port, console.log(`server is listening on port ${port}`));
+const start = async () => {
+  try {
+    await connectDB();
+    app.listen(port, console.log(`server is listening on port ${port}`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
--- task-manager/db/connect.js
+++ task-manager/db/connect.js
@@ -3,11 +3,8 @@ const mongoose = require("mongoose");
 const connectionString =
   "mongodb+srv://expressapps:1234@cluster0.wwugs.mongodb.net/task-manager?retryWrites=true&w=majority";
 
-mongoose
-  .connect(connectionString)
-  .then(() => {
-    console.log("CONNECTED TO DB");
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+const connectDB = (url) => {
+  return mongoose.connect(connectionString);
+};
+
+module.exports = connectDB;
