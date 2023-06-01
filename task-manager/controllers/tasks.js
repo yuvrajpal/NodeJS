75c1960dc4d34dbd742cac0e77eb626629d5e71b
Yuvraj Pal <techyuvraj@gmail.com>
2023-06-02 00:37:55 +0530

    first schema

--- task-manager/controllers/tasks.js
+++ task-manager/controllers/tasks.js
@@ -1,9 +1,12 @@
+const Task = require("../models/Task");
+
 const getAllTasks = (req, res) => {
   res.send("all items from file");
 };
 
-const createTask = (req, res) => {
-  res.json(req.body);
+const createTask = async (req, res) => {
+  const task = await Task.create(req.body);
+  res.status(210).json({ task });
 };
 
 const getTask = (req, res) => {
